0098fa657ae9f0cb9d8e398b7c0ea2b6b64ee27c
GuilhermeBarboosa <guilherme.strg@gmail.com>
2022-06-15 11:13:07 -0300

    fix: TRY CATCH NO METODO GUI

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -16,17 +16,22 @@ public class Gui {
     Scanner ler = new Scanner(System.in);
 
     public int menu() {
-        int opc;
-        System.out.println("MENU DE OPÇÕES\n");
-        System.out.println("1 - REGISTRAR");
-        System.out.println("2 - MODIFICAR");
-        System.out.println("3 - MOSTRAR");
-        System.out.println("4 - EXCLUIR");
-        System.out.println("ESCOLHA A OPÇÃO: ");
-        opc = Integer.parseInt(ler.nextLine());
-        return opc;
+        try {
+            int opc;
+            System.out.println("MENU DE OPÇÕES\n");
+            System.out.println("1 - REGISTRAR");
+            System.out.println("2 - MODIFICAR");
+            System.out.println("3 - MOSTRAR");
+            System.out.println("4 - EXCLUIR");
+            System.out.println("ESCOLHA A OPÇÃO: ");
+            opc = Integer.parseInt(ler.nextLine());
+            return opc;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return -1;
     }
-    
+
     public String[] login() {
         String[] loginSenha = new String[2];
         System.out.println("Login:");
@@ -37,43 +42,54 @@ public class Gui {
     }
 
     public int escolherCRUD() {
-        int opc;
-        System.out.println("MENU DE OPÇÕES\n");
-        System.out.println("1 - CAMPUS");
-        System.out.println("2 - SERVIDOR");
-        System.out.println("3 - CURSO");
-        System.out.println("4 - DISCIPLINA");
-        System.out.println("5 - OFERTA DE DISCIPLINA");
-        System.out.println("6 - ORIENTAÇÃO");
-        System.out.println("7 - ATIVIDADE");
-        System.out.println("8 - COMISSÕES");
-        System.out.println("9 - VINCULO DE SERVIDORES A COMISSÃO");
-        System.out.println("10 - REUNIÃO");
-        System.out.println("11 - REUNIÃO E PRESENTES");
-        System.out.println("12 - RELATÓRIOS");
-        System.out.println("13 - ENCERRAR COMISSAO");
-        System.out.println("14 - SAIR DO MENU");
-        System.out.println("ESCOLHA A OPÇÃO: ");
-        opc = Integer.parseInt(ler.nextLine());
-        return opc;
+
+        try {
+            int opc;
+            System.out.println("MENU DE OPÇÕES\n");
+            System.out.println("1 - CAMPUS");
+            System.out.println("2 - SERVIDOR");
+            System.out.println("3 - CURSO");
+            System.out.println("4 - DISCIPLINA");
+            System.out.println("5 - OFERTA DE DISCIPLINA");
+            System.out.println("6 - ORIENTAÇÃO");
+            System.out.println("7 - ATIVIDADE");
+            System.out.println("8 - COMISSÕES");
+            System.out.println("9 - VINCULO DE SERVIDORES A COMISSÃO");
+            System.out.println("10 - REUNIÃO");
+            System.out.println("11 - REUNIÃO E PRESENTES");
+            System.out.println("12 - RELATÓRIOS");
+            System.out.println("13 - ENCERRAR COMISSAO");
+            System.out.println("14 - SAIR DO MENU");
+            System.out.println("ESCOLHA A OPÇÃO: ");
+            opc = Integer.parseInt(ler.nextLine());
+            return opc;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return -1;
     }
-    
+
     public int menuComum() {
-        int opc;
-        System.out.println("MENU DE OPÇÕES\n");
-        System.out.println("1 - CAMPUS");
-        System.out.println("2 - SERVIDOR");
-        System.out.println("3 - CURSO");
-        System.out.println("4 - DISCIPLINA");
-        System.out.println("5 - OFERTA DE DISCIPLINA");
-        System.out.println("6 - ORIENTAÇÃO");
-        System.out.println("7 - ATIVIDADE");
-        System.out.println("8 - COMISSÕES");
-        System.out.println("10 - REUNIÃO");
-        System.out.println("11 - REUNIÃO E PRESENTES");
-        System.out.println("12 - SAIR");
-        opc = Integer.parseInt(ler.nextLine());
-        return opc;
+        try {
+            int opc;
+            System.out.println("MENU DE OPÇÕES\n");
+            System.out.println("1 - CAMPUS");
+            System.out.println("2 - SERVIDOR");
+            System.out.println("3 - CURSO");
+            System.out.println("4 - DISCIPLINA");
+            System.out.println("5 - OFERTA DE DISCIPLINA");
+            System.out.println("6 - ORIENTAÇÃO");
+            System.out.println("7 - ATIVIDADE");
+            System.out.println("8 - COMISSÕES");
+            System.out.println("10 - REUNIÃO");
+            System.out.println("11 - REUNIÃO E PRESENTES");
+            System.out.println("12 - SAIR");
+            opc = Integer.parseInt(ler.nextLine());
+            return opc;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return -1;
     }
 
     public void sucess() {
